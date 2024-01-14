068caa91d84c733da82d62d1644e7bad683b1b6d
webNekit <mochalov-hunter@mail.ru>
2024-01-14 21:55:02 +0300

    add typography settings style

--- client-blog/src/app/layout.jsx
+++ client-blog/src/app/layout.jsx
@@ -1,7 +1,8 @@
-import { Inter } from 'next/font/google'; 
+import { Lato, Open_Sans } from 'next/font/google'; 
 import '@/styles/main.sass';
 
-const inter = Inter({ subsets: ['latin'] })
+const lato = Lato({ subsets: ['latin'], weight: ['400', '700', '900'], variable: '--font-lato' });
+const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' });
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${openSans.className} ${openSans.variable} ${lato.variable}`}>{children}</body>
     </html>
   )
 }
