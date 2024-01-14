4ebb366e8c16f3544262929a7b0c2fe291009b3c
webNekit <mochalov-hunter@mail.ru>
2024-01-15 00:07:53 +0300

    create button component and responsive card

--- client-blog/src/components/card/card.jsx
+++ client-blog/src/components/card/card.jsx
@@ -1,17 +1,21 @@
 import Image from 'next/image';
 import styles from '@/components/card/card.module.sass';
+import Button, { IconTypes } from '@/components/button/button';
 
 export default function card() {
   return (
-    <div className={styles.card}>
+    <div className={styles.card_list}>
+      <div className={styles.card}>
         <div className={styles.card_imageWrap}>
-            <Image src="/img_1.jpeg" className={styles.card_image} alt="" fill={true} />
+          <Image src="/img_1.jpeg" className={styles.card_image} alt="" fill={true} />
         </div>
         <div className={styles.card_contentWrap}>
-            <div className={`${styles.card_label} h6 mb-10 c-orange`}>Product reviews</div>
-            <h3 className={`${styles.card_title} h3 mb-20`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quae.</h3>
-            <p className={`${styles.card_text} mb-20`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis repellat dolorem nemo libero. Natus similique ab, ipsam illum harum earum!</p>
+          <div className={`${styles.card_label} h6 mb-10 c-orange`}>Product reviews</div>
+          <h3 className={`${styles.card_title} h3 mb-20`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quae.</h3>
+          <p className={`${styles.card_text} mb-20`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis repellat dolorem nemo libero. Natus similique ab, ipsam illum harum earum!</p>
+          <Button href="#" icon={IconTypes.ARROW_RIGHT}>Read More</Button>
         </div>
+      </div>
     </div>
   )
 }
--- client-blog/src/components/header/header.jsx
+++ client-blog/src/components/header/header.jsx
@@ -2,7 +2,7 @@ export default function header() {
   return (
     <header>
         <div className="container">
-            <h1 className="title h1">Твой блог</h1>
+            <h1 className="title h1 pt-80 pb-80">Твой блог</h1>
         </div>
     </header>
   )
