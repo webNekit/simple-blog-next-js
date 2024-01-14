d1cea7b0d2fb812a0f4ec6ef09f8a2c8f4bc558b
webNekit <mochalov-hunter@mail.ru>
2024-01-14 23:01:45 +0300

    add styles for component card

--- client-blog/src/components/card/card.jsx
+++ client-blog/src/components/card/card.jsx
@@ -8,9 +8,9 @@ export default function card() {
             <Image src="/img_1.jpeg" className={styles.card_image} alt="" fill={true} />
         </div>
         <div className={styles.card_contentWrap}>
-            <div className={styles.card_label}>Product reviews</div>
-            <h3 className={`${styles.card_title} h3`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quae.</h3>
-            <p className={styles.card_text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis repellat dolorem nemo libero. Natus similique ab, ipsam illum harum earum!</p>
+            <div className={`${styles.card_label} h6 mb-10 c-orange`}>Product reviews</div>
+            <h3 className={`${styles.card_title} h3 mb-20`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quae.</h3>
+            <p className={`${styles.card_text} mb-20`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis repellat dolorem nemo libero. Natus similique ab, ipsam illum harum earum!</p>
         </div>
     </div>
   )
