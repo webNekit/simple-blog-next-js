a209e1e0b06f2bfff4a8aadb552e524aee976d9f
webNekit <mochalov-hunter@mail.ru>
2024-01-14 21:34:38 +0300

    Add settings style in sass

--- client-blog/src/app/layout.jsx
+++ client-blog/src/app/layout.jsx
@@ -1,5 +1,5 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google'; 
+import '@/styles/main.sass';
 
 const inter = Inter({ subsets: ['latin'] })
 
